e1a6f15a6cdcc1d05cf6caa362fad5e9c1cf1df3
Aryan Patel <108725292+aryan2107-cloud@users.noreply.github.com>
2026-03-27 15:52:17 -0400

    Refactor summarization using pipeline from transformers

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import pipeline
 import gradio as gr
 
-model_name = "sshleifer/distilbart-cnn-12-6"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+model = pipeline("summarization")
 
 def predict(prompt):
-    inputs = tokenizer(prompt, return_tensors="pt", max_length=1024, truncation=True)
-    summary_ids = model.generate(inputs["input_ids"], max_length=150)
-    return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    summary = model(prompt)[0]["summary_text"]
+    return summary
 
-with gr.Blocks() as demo:
-    textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
-    gr.Interface(fn=predict, inputs=textbox, outputs="text")
-
+demo = gr.Interface(fn=predict, inputs="textbox", outputs="text")
 demo.launch()
