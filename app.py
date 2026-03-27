83bcd9678aee789f7b5029ecc5c379949bf66c47
Aryan Patel <108725292+aryan2107-cloud@users.noreply.github.com>
2026-03-27 16:11:22 +0000

    Updated all the files

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from transformers import pipeline 
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import gradio as gr
 
-model = pipeline("summarization")
+model_name = "sshleifer/distilbart-cnn-12-6"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
 def predict(prompt):
-    summary = model(prompt) [0] ("summary_text")
-    return summary
+    inputs = tokenizer(prompt, return_tensors="pt", max_length=1024, truncation=True)
+    summary_ids = model.generate(inputs["input_ids"], max_length=150)
+    return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
 with gr.Blocks() as demo:
     textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
-    gr.Interface(fn=predict, inputs=textbox, outputs='text')
+    gr.Interface(fn=predict, inputs=textbox, outputs="text")
 
 demo.launch()
